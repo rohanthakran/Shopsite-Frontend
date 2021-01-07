f1b8234f55c0ea270f851b0911b31ed8decb91bb
Rohan <31851961+rohanthakran@users.noreply.github.com>
2021-01-07 18:21:56 +0530

    Add files via upload

---
+++ precache-manifest.538021bc1403f57ca58c870482c400df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e711b26d108558257389f00a874585",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20e2e5af6417318395db",
+    "url": "/static/css/main.d38a6a2f.chunk.css"
+  },
+  {
+    "revision": "e5445538e1d0a64f2f2b",
+    "url": "/static/js/2.5b7f77a6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5b7f77a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20e2e5af6417318395db",
+    "url": "/static/js/main.33c22ffc.chunk.js"
+  },
+  {
+    "revision": "d409c5ad447759d6652b",
+    "url": "/static/js/runtime-main.f26a748b.js"
+  }
+]);
